9024bbd22c712b61920209c6a3b53b675d257fe2
Pedro Henrique Paulino Pires <121137670+pedropaulinopires@users.noreply.github.com>
2023-07-28 15:43:14 -0300

    começando o template de menu

---
+++ src/main/resources/pre-templates/btnSessaoScripts.js
@@ -0,0 +1,25 @@
+const btnSessao = document.getElementById("btnSessao");
+const listaSessoesMobile = document.getElementById("listaSessoesMobile")
+const main = [...document.getElementsByTagName("main")]
+
+function fecharSessao(){
+    if(btnSessao.classList.contains("active")){
+        btnSessao.classList.remove("active")
+        listaSessoesMobile.classList.remove("active")
+    }
+}
+
+
+btnSessao.addEventListener("click",()=>{
+    btnSessao.classList.toggle("active")
+    if(btnSessao.classList.contains("active")){
+        listaSessoesMobile.classList.add("active")
+    } else {
+        listaSessoesMobile.classList.remove("active")
+    }
+})
+
+main.map(ele => ele.addEventListener("click",fecharSessao))
+
+
+
