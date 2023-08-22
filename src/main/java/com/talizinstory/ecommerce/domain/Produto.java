6b2e85a77ae18f8c53ed52adec9a8649937c2574
Pedro Henrique Paulino Pires <121137670+pedropaulinopires@users.noreply.github.com>
2023-08-21 21:46:33 -0300

    atualizando a modelagem do sistema de um ecommerce para afiliado!

--- src/main/java/com/talizinstory/ecommerce/domain/Produto.java
+++ src/main/java/com/talizinstory/ecommerce/domain/Produto.java
@@ -27,22 +27,12 @@ public class Produto {
     @Column(nullable = false, unique = true, length = 150)
     private String nome;
 
-    @Column(nullable = false)
-    private Integer quantidadeEstoque;
-
-    @Column(nullable = false)
-    private BigDecimal valor;
-
-    @Column(nullable = false)
-    private BooleanEnum promocao;
 
     @ManyToMany
     @JoinTable(name = "produto_sessoes", joinColumns = {@JoinColumn(name = "produto_id", referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "sessao.id", referencedColumnName = "id")})
     private List<Sessao> sessoes;
-
-    @Column(nullable = false)
-    private BigDecimal valorNovo;
+    
 
     @Column(nullable = false)
     private String imagemPrincipal;
@@ -58,8 +48,7 @@ public class Produto {
     @Column(nullable = false, length = 600)
     private String caracteristicas;
 
-    @Column(nullable = false)
-    private List<TipoEnvioEnum> tipoEnvio;
+
 
 
 }
