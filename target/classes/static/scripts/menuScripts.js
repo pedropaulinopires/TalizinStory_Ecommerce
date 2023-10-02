7b144911eba8780bcf0714621e8d14a433baf16f
Pedro Henrique Paulino Pires <121137670+pedropaulinopires@users.noreply.github.com>
2023-10-02 20:24:17 -0300

    comit

--- target/classes/static/scripts/menuScripts.js
+++ target/classes/static/scripts/menuScripts.js
@@ -1,60 +1,9 @@
-/*menu mobile*/
-const btnSessao = document.getElementById("btnSessao");
-const listaSessoesMobile = document.getElementById("listaSessoesMobile")
-const tabelaSessao = document.getElementById("tabelaSessao")
-const main = [...document.getElementsByTagName("main")]
-
-/*menu desktop*/
-const menuDesktop = document.getElementById("menuDesktop")
-const btnBuscaDesktop = document.getElementById("btnBuscaDesktop")
-const sessaoDesktop = document.getElementById("sessaoDesktop")
-const listaSessaoDesktop = document.getElementById("listaSessaoDesktop")
-
-
-function resetComponents(){
-    if(btnBuscaDesktop.classList.contains("active")){
-        btnBuscaDesktop.classList.remove("active")
-        sessaoDesktop.classList.remove("active")
-    }
-    if(btnSessao.classList.contains("active")){
-        btnSessao.classList.remove("active")
-        listaSessoesMobile.classList.remove("active")
-    }
-}
-
-function sessaoDesktopEvents(){
-    btnBuscaDesktop.classList.toggle("active")
-    if(btnBuscaDesktop.classList.contains("active")){
-        sessaoDesktop.classList.add("active")
-    } else {
-        sessaoDesktop.classList.remove("active")
-    }
-}
-
-
-btnSessao.addEventListener("click",()=>{
-    btnSessao.classList.toggle("active")
-    if(btnSessao.classList.contains("active")){
-        listaSessoesMobile.classList.add("active")
-    } else {
-        listaSessoesMobile.classList.remove("active")
-    }
-})
-
-main.map(ele => ele.addEventListener("click",resetComponents));
-
-[...tabelaSessao.children].map(ele => ele.addEventListener("click",resetComponents));
-[...listaSessaoDesktop.children].map(ele => ele.addEventListener("click",resetComponents))
-
-btnBuscaDesktop.addEventListener("click",sessaoDesktopEvents)
-window.addEventListener("resize",resetComponents)
+let menuDesktop = document.getElementById("menuDesktop");
 window.addEventListener("scroll",() =>{
-    if (window.pageYOffset > 50) {
-        menuDesktop.style.padding = "0px 60px"
-        sessaoDesktop.style.top = "50px"
-        
+    if (window.pageYOffset > 40) {
+        menuDesktop.classList.add("active")
+
     } else {
-        menuDesktop.style.padding = "20px 60px"
-        sessaoDesktop.style.top = "90px"
+        menuDesktop.classList.remove("active")
     }
 })
